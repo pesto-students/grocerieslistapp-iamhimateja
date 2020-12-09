87b1087da03c86870d978691546a3d87c627eada
Himateja Merlapaka <iamhimateja@gmail.com>
2020-12-09 19:25:51 +0530

    WIP: Edit Grocery Item

--- scripts/app.js
+++ scripts/app.js
@@ -1,110 +1,259 @@
 // TODO: grocery item - CRUD actions
 // TODO: Validations
+"use strict";
 
-'use strict';
+const randomKey = () => Math.random().toString(36).substr(2, 5);
 
-const userData = () => {
-  return JSON.parse(localStorage.getItem("users"));
-}
+function App(maxItems) {
+  let $this = this;
+  $this.loadDummyData = async () => {
+    const dummyData = await fetch("../data/users.json")
+      .then((response) => response.json())
+      .then((data) => {
+        let groupedData = localStorage.getItem("users") ? $this.users() : {};
+        data.forEach((element, index) => {
+          if (!groupedData[element.name]) {
+            groupedData[element.name] = {
+              name: element.name,
+              groceryList: {}
+            };
+            element.groceryList.forEach(function (groceryItem, index) {
+              groupedData[element.name].groceryList[
+                randomKey()
+              ] = groceryItem;
+            });
+          }
+        });
+        localStorage.setItem("users", JSON.stringify(groupedData));
+      });
+    return localStorage.getItem("users");
+  };
 
-const currentUserData = () => {
-  if (sessionStorage.getItem("currentUser")) {
-    return users[sessionStorage.getItem("currentUser")]
-  }
-}
+  $this.loadDummyData();
+  $this.users = () => JSON.parse(localStorage.getItem("users")) || {};
 
-const getUser = (username) => {
-  return users[username]
-}
+  $this.currentUser = () => {
+    if (sessionStorage.getItem("currentUser")) {
+      return $this.users()[sessionStorage.getItem("currentUser")];
+    }
+  };
 
-const isUserExists = (username) => {
-  return users[username]
-}
+  $this.findUser = (username) => {
+    return $this.users()[username];
+  };
 
-const LogIn = (username) => {
-  sessionStorage.setItem("currentUser", username)
-  window.currentUser = currentUserData();
-}
+  $this.findGrocerylistItem = (id) => {
+    return $this.currentUser().groceryList[id];
+  };
 
-const toggleLoginScreen = (toggle = "open") => {
-  switch (toggle) {
-    case "open":
-      loginContainer.classList.remove("close")
-      break;
-    case "close":
-      loginContainer.classList.add("close")
-      break;
-  }
-}
+  $this.LogIn = (username) => {
+    sessionStorage.setItem("currentUser", username);
+    document.getElementById("currrent-user-name").innerHTML = `${
+      $this.currentUser().name
+    }'s`;
+    $this.addGroceryItemsToUI();
+  };
 
-const createNewUserAndLogIn = (username) => {
-  users[username] = {
-    name: username,
-    goceryList: []
-  }
-  localStorage.setItem("users", JSON.stringify(users));
-  window.users = userData();
-  LogIn(username)
-  window.currentUser = currentUserData();
-  return users[username];
-}
+  $this.LogOut = () => {
+    sessionStorage.removeItem("currentUser");
+    $this.toggleLoginScreen("open");
+    $this.toggleGroceryScreen("close");
+  };
 
-const loadDummyData = async () => {
-  const dummyData = await fetch("../data/users.json").then(response => response.json()).then(data => {
-    let groupedData = localStorage.getItem("users") ? JSON.parse(localStorage.getItem("users")) : {}
-    data.forEach((element, index) => {
-      if (!groupedData[element.name]) {
-        groupedData[element.name] = element
-      }
-    });
-    localStorage.setItem("users", JSON.stringify(groupedData));
-  })
-  return dummyData;
-}
+  $this.createNewUserAndLogIn = (username) => {
+    let data = $this.users();
+    data[username] = {
+      name: username,
+      groceryList: []
+    };
+    localStorage.setItem("users", JSON.stringify(data));
+    $this.LogIn(username);
+    return $this.findUser[username];
+  };
 
-const addGroceryItem = (itemName) => {
-  if (itemName == "")
-    throw "Item name shouldn't be blank" 
+  $this.toggleLoginScreen = (toggle = "open") => {
+    switch (toggle) {
+      case "open":
+        loginContainer.classList.remove("close");
+        break;
+      case "close":
+        loginContainer.classList.add("close");
+        break;
+    }
+  };
 
-  currentUser.goceryList.push({
-  })
+  $this.addGroceryItemsToUI = () => {
+    const groceryList = $this.currentUser().groceryList;
+    groceriesListElement.innerHTML = "";
+    for (let key in groceryList) {
+      $this.addGroceryItemAndBindEvents(key, groceryList[key]);
+    }
+  };
+
+  $this.addGroceryItemAndBindEvents = (id, groceryItem) => {
+    let listElement = document.createElement("li");
+    let listLabel = document.createElement("label");
+    let listLabelCheckBox = document.createElement("input");
+    let listLabelNameElement = document.createElement("span");
+    let actionButtonsWrap = document.createElement("div");
+    let editButton = document.createElement("button");
+    let deleteButton = document.createElement("button");
+    listElement.classList += "grocery-list-item";
+    listElement.setAttribute("data-id", id);
+    listLabelCheckBox.type = "checkbox";
+    listLabelCheckBox.name = "groceryItem";
+    listLabelCheckBox.value = id;
+    listLabelNameElement.classList += "item-name";
+    actionButtonsWrap.classList += "action-buttons";
+    editButton.classList += "btn edit";
+    deleteButton.classList += "btn delete";
+    editButton.setAttribute("data-id", id);
+    deleteButton.setAttribute("data-id", id);
+    listLabelNameElement.innerHTML = groceryItem.itemName;
+    editButton.innerHTML = "Edit";
+    deleteButton.innerHTML = "Delete";
+    if (groceryItem.completed) {
+      listLabelCheckBox.checked = groceryItem.completed;
+      listElement.classList += " completed";
+    }
+    listLabel.append(listLabelCheckBox);
+    listLabel.append(listLabelNameElement);
+    listElement.append(listLabel);
+    actionButtonsWrap.append(editButton);
+    actionButtonsWrap.append(deleteButton);
+    listElement.append(actionButtonsWrap);
+    groceriesListElement.append(listElement);
+
+    listElement
+      .querySelector("input[type='checkbox']")
+      .addEventListener("change", function (event) {
+        const targetElement = document.querySelector(
+          ".grocery-list-item[data-id='" + this.value + "']"
+        );
+        $this.toggleGroceryItemCompletedStatus(this.checked, this.value);
+        if (this.checked) {
+          targetElement.classList.add("completed");
+        } else {
+          targetElement.classList.remove("completed");
+        }
+      });
+
+    listElement
+      .querySelector(".btn.delete")
+      .addEventListener("click", function (event) {
+        const itemKey = this.dataset.id;
+        $this.deleteGroceryItem(itemKey);
+      });
+  };
+
+  $this.toggleGroceryScreen = (toggle = "open") => {
+    switch (toggle) {
+      case "open":
+        groceryScreen.classList.add("show");
+        break;
+      case "close":
+        groceryScreen.classList.remove("show");
+        break;
+    }
+  };
+
+  $this.addGroceryItem = (itemName) => {
+    if (itemName == "") throw "Item name shouldn't be blank";
+    const itemObject = {
+      itemName: itemName,
+      completed: false
+    };
+    let currentUser = $this.currentUser();
+    let usersList = $this.users();
+    let itemKey = randomKey();
+    usersList[currentUser.name].groceryList[itemKey] = itemObject;
+    localStorage.setItem("users", JSON.stringify(usersList));
+    $this.addGroceryItemAndBindEvents(itemKey, itemObject);
+  };
+
+  $this.deleteGroceryItem = (itemKey) => {
+    let usersList = $this.users();
+    let currentUser = usersList[$this.currentUser().name];
+    let groceryItem = currentUser.groceryList[itemKey];
+    if (groceryItem) {
+      delete currentUser.groceryList[itemKey];
+    }
+    $this.deleteGroceryItemFromUI(itemKey);
+    localStorage.setItem("users", JSON.stringify(usersList));
+  };
+
+  $this.deleteGroceryItemFromUI = (itemKey) => {
+    const groceryItem = document.querySelector(
+      ".grocery-list-item[data-id='" + itemKey + "']"
+    );
+    groceryItem.remove();
+  };
+
+  $this.toggleGroceryItemCompletedStatus = (checked, itemKey) => {
+    let usersList = $this.users();
+    let currentUser = usersList[$this.currentUser().name];
+    let groceryItem = currentUser.groceryList[itemKey];
+    if (groceryItem) {
+      groceryItem.completed = checked;
+    }
+    localStorage.setItem("users", JSON.stringify(usersList));
+  };
 }
 
 window.addEventListener("load", function () {
-  loadDummyData()
-  window.users = userData();
-  window.currentUser = currentUserData();
+  const app = new App(5);
+  console.log(app.currentUser());
   window.loginContainer = document.getElementById("login");
+  window.groceryScreen = document.getElementById("grocery-list-wrapper");
+  window.groceriesListElement = document.getElementById("groceries-list");
   const usernameInput = document.getElementById("username");
-  usernameInput.addEventListener("keyup", function(event) {
+  usernameInput.addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
       if (this.value.length == 0) {
         console.log("Name is not valid");
       } else {
-        if (isUserExists(this.value)) {
-          LogIn(this.value)
-          toggleLoginScreen('close')
+        if (app.findUser(this.value)) {
+          app.LogIn(this.value);
+          app.toggleLoginScreen("close");
+          app.toggleGroceryScreen("open");
         } else {
-          createNewUserAndLogIn(this.value)
-          toggleLoginScreen('close')
+          app.createNewUserAndLogIn(this.value);
+          app.toggleLoginScreen("close");
+          app.toggleGroceryScreen("open");
         }
       }
     }
   });
 
-  document.getElementById("item-name").addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-      if (this.value.length == 0) {
+  document
+    .getElementById("item-name")
+    .addEventListener("keyup", function (event) {
+      if (event.keyCode === 13) {
+        document.getElementById("add-item").click();
+      }
+    });
+
+  document
+    .getElementById("add-item")
+    .addEventListener("click", function (event) {
+      const inputElement = document.getElementById("item-name");
+      if (inputElement.value.length == 0) {
         console.log("Item name shouldn't be blank.");
       } else {
-        addGroceryItem(this.value)
+        app.addGroceryItem(inputElement.value);
+        inputElement.value = "";
       }
-    }
+    });
+
+  document.getElementById("logout").addEventListener("click", function (event) {
+    app.LogOut();
   });
 
-  if (currentUser) {
-    toggleLoginScreen('close')
-    document.getElementById("currrent-user-name").innerHTML = `${currentUser.name}'s`
-    // alert(currentUser.name + " Already signed in")
+  if (app.currentUser()) {
+    app.toggleLoginScreen("close");
+    app.toggleGroceryScreen("open");
+    app.LogIn(app.currentUser().name);
+  } else {
+    app.toggleLoginScreen();
   }
-})
+});
